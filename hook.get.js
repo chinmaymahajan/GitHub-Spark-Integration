3ebe019651c1d7db50d50faa25d1864a998d351f
Chinmay <chimahaj@cisco..com>
2017-11-05 12:58:07 -0500

    fixed indentation

--- hook.get.js
+++ hook.get.js
@@ -10,17 +10,17 @@ module['exports'] = function myService (req, res, next) {
 	req = hook.req,
 	store = hook.datastore;
 	var message = '';
-  	if (hook.params.action === "opened")
-   		    message = "This pull request is open " + hook.params.action.pull_request.url;
+	if (hook.params.action === "opened")
+		message = "This pull request is open " + hook.params.action.pull_request.url;
     // if (hook.params.action === "submitted")
     //  	message = "PR review subimitted for " + pull_request;
   	if (hook.params.action === "review_requested") {
 
-		message = "Review is requested for this PR " 
-		+ hook.params.pull_request.title + " - " 
-		+ hook.params.pull_request.html_url + " from " 
+		message = "Review is requested for this PR "
+		+ hook.params.pull_request.title + " - "
+		+ hook.params.pull_request.html_url + " from "
 		+ hook.params.pull_request.user.login;
-    }
+	}
       /* axios.get(hook.params.pull_request.user.login.comments_url)
       .then(function (response) {
       message = message + " Here is the response " + response;
@@ -38,13 +38,13 @@ module['exports'] = function myService (req, res, next) {
 
 	if (hook.params.action === "created") {
 		var userEmail =  hook.params.pull_request.user.login + "@cisco.com";
-		message = "Review comment " 
+		message = "Review comment "
 			+ hook.params.comment.body
 			+ " from user "
 			+ hook.params.comment.user.login
 			+ "On his PR " + userEmail;
- 
-  
+
+
     axios.get('https://api.ciscospark.com/v1/people', {
       		email: userEmail
 		},
@@ -54,21 +54,20 @@ module['exports'] = function myService (req, res, next) {
 				'Content-Type': 'application/json; charset=utf-8', 'Authorization': acessToken
 			}
     }) //get request for people
-    
-      .then(function (personDetails) {
-      
-    var personID = personDetails.items[0].id;
-  	axios.post('https://api.ciscospark.com/v1/messages', {
-      		toPersonId: personID,
-      		text: message
-		},
-		{
-			headers:
-			{
-				'Content-Type': 'application/json; charset=utf-8', 'Authorization': acessToken
-			}
-    	})
 
+      .then(function (personDetails) {
+
+    var personID = personDetails.items[0].id;
+				axios.post('https://api.ciscospark.com/v1/messages', {
+					toPersonId: personID,
+					text: message
+				},
+				{
+					headers:
+				{
+					'Content-Type': 'application/json; charset=utf-8', 'Authorization': acessToken
+				}
+				})
 					.then(function (response) {
 						if (response.status === 200) {
 							console.log("personId is ", personID);
@@ -77,14 +76,14 @@ module['exports'] = function myService (req, res, next) {
 					})
 					.catch((error) => {
 						assert.isNotOk(error,'Promise Persondetails  error');
-			 			 done();
+						done();
 					});
 			}) //.then
 			.catch((error) => {
-			  assert.isNotOk(error,'Promise Persondetails  error');
-			  done();
+				assert.isNotOk(error,'Promise Persondetails  error');
+				done();
 			});
-	} //if to send individual messages 
+	} //if to send individual messages
 
 	//	hook.res.end(hook.params);
 	axios.post('https://api.ciscospark.com/v1/messages',
